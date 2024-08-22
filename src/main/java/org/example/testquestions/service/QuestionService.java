34920a1a9f90b8f71bc69f31f571ac465fa27405
Asus Zenbook <anapiiaevsandary@gmail.com>
2024-08-22 22:35:38 +0300

    Testing

---
+++ src/main/java/org/example/testquestions/service/QuestionService.java
@@ -0,0 +1,45 @@
+package org.example.testquestions.service;
+
+import org.example.testquestions.entity.TestQuestion;
+import org.example.testquestions.repository.QuestionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class QuestionService {
+
+   QuestionRepository repository;
+
+    public List<TestQuestion> getAllQuestions() {
+        return repository.findAll();
+    }
+
+    public Optional<TestQuestion> getQuestionById(Long id) {
+        return repository.findById(id);
+    }
+
+    public TestQuestion addQuestion(TestQuestion question) {
+        return repository.save(question);
+    }
+
+    public Optional<TestQuestion> updateQuestion(Long id, TestQuestion newQuestion) {
+        return repository.findById(id).map(question -> {
+            question.setQuestion(newQuestion.getQuestion());
+            question.setOptionA(newQuestion.getOptionA());
+            question.setOptionB(newQuestion.getOptionB());
+            question.setOptionC(newQuestion.getOptionC());
+            question.setOptionD(newQuestion.getOptionD());
+            question.setCorrectOption(newQuestion.getCorrectOption());
+            return repository.save(question);
+        });
+    }
+
+    public boolean deleteQuestion(Long id) {
+        return repository.findById(id).map(question -> {
+            repository.delete(question);
+            return true;
+        }).orElse(false);
+    }
+}
